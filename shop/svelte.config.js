308291b5dab46d543e6bb32d2ec3eaa41bebfabc
Neville Brem <bremneville@gmail.com>
2024-02-13 08:03:38 +0100

    refacor: moved to monorepo structure

--- shop/svelte.config.js
+++ shop/svelte.config.js
@@ -3,10 +3,14 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	extensions: ['.svelte'],
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
-	preprocess: [vitePreprocess({})],
-
+	preprocess: [vitePreprocess()],
+	
+	vitePlugin: {
+		inspector: true,
+	},
 	kit: {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
@@ -14,5 +18,4 @@ const config = {
 		adapter: adapter()
 	}
 };
-
 export default config;
