716a87e4c1dc82bc0a77933c63d9d26e4f1168c1
Neville Brem <bremneville@gmail.com>
2024-02-14 16:56:35 +0100

    added drawer

--- shop/src/routes/+layout.svelte
+++ shop/src/routes/+layout.svelte
@@ -2,8 +2,12 @@
 	import '../app.pcss';
 	import Footer from '$lib/components/Footer.svelte';
 	import Nav from '$lib/components/Nav.svelte';
+	import { Drawer, getDrawerStore, initializeStores } from '@skeletonlabs/skeleton';
+
+	initializeStores();
 </script>
 
+<Drawer />
 <Nav />
 <slot />
 <Footer />
