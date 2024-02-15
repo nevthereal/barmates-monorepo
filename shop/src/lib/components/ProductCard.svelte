f79eff0f091b91c90fff64704b8cad727a4cdf4e
Neville Brem <bremneville@gmail.com>
2024-02-15 09:14:20 +0100

    added productcard

---
+++ shop/src/lib/components/ProductCard.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import Logo from '$lib/assets/LOGO.png';
+</script>
+
+<div class="card btn p-4">
+	<a href="/">
+		<img class="p-2 h-56 mx-auto rounded-3xl" src={Logo} alt="product" loading="lazy" />
+		<h3 class="font-bold h3 text-center">Title</h3>
+		<h4 class="font-bold h4">10000 CHF</h4>
+	</a>
+</div>
---
+++ shop/src/routes/store/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+	import ProductCard from '$lib/components/ProductCard.svelte';
+</script>
+
+<main class="mx-16 mt-16">
+	<h1 class="h1 font-black text-primary-500 my-16">Store</h1>
+	<div class="grid grid-cols-4 gap-4">
+		<ProductCard />
+		<ProductCard />
+		<ProductCard />
+		<ProductCard />
+		<ProductCard />
+		<ProductCard />
+		<ProductCard />
+		<ProductCard />
+		<ProductCard />
+	</div>
+</main>
