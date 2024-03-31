de96d8ef8b4d4f7a1446c2378f287ed42bc3290e
Neville Brem <bremneville@gmail.com>
2024-03-31 12:13:10 +0200

    tweaked output

--- home/astro.config.mjs
+++ home/astro.config.mjs
@@ -8,6 +8,6 @@ import svelte from "@astrojs/svelte";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react(), svelte()],
-  output: "server",
+  output: "static",
   adapter: vercel(),
 });
