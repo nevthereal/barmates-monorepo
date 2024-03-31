d7798f6587de090f49b8da7e669d138214f0b305
Neville Brem <bremneville@gmail.com>
2024-03-31 12:11:17 +0200

    added static adapter

--- home/astro.config.mjs
+++ home/astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-import vercel from "@astrojs/vercel";
+import vercel from "@astrojs/vercel/static";
 
 import svelte from "@astrojs/svelte";
 
