6024c2b7bf296f3e72f18913ad5cc89aa73c69bb
Neville Brem <bremneville@gmail.com>
2024-03-31 16:28:39 +0200

    db

--- shop/drizzle.config.ts
+++ shop/drizzle.config.ts
@@ -8,5 +8,6 @@ export default {
 	dbCredentials: {
 		url: process.env.DB_URL ?? '',
 		authToken: process.env.DB_AUTH_TOKEN
-	}
+	},
+	verbose: true
 } satisfies Config;
--- shop/src/lib/server/db/db.ts
+++ shop/src/lib/server/db/db.ts
@@ -1,3 +1,4 @@
+import 'dotenv/config';
 import { createClient } from '@libsql/client';
 import { drizzle } from 'drizzle-orm/libsql';
 import * as schema from './schema';
