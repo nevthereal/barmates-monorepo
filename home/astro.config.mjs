68d9e5327a035b17e67c56a6625b771a2c37c278
Neville Brem <bremneville@gmail.com>
2024-03-31 11:41:45 +0200

    added vercel adapter

--- home/astro.config.mjs
+++ home/astro.config.mjs
@@ -1,7 +1,7 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-import netlify from "@astrojs/netlify";
+import vercel from "@astrojs/vercel";
 
 import svelte from "@astrojs/svelte";
 
@@ -9,5 +9,5 @@ import svelte from "@astrojs/svelte";
 export default defineConfig({
   integrations: [tailwind(), react(), svelte()],
   output: "server",
-  adapter: netlify()
+  adapter: vercel(),
 });
